96dc92527e1662a316e996e9294dfc395c3075fd
Essoh Jean Penuel <penuel225@gmail.com>
2022-07-19 11:31:37 +0000

    Annuler la base de donnees

--- app.js
+++ app.js
@@ -11,20 +11,27 @@ const db = require('./db/database');
 
 
 
-db.connect(function(err){
-    if (err) {
-        console.log(err)
-    }else{
-        app.use('/rejoindre', rejoindre);
-        app.use('/contact', contact);
-        app.use('/connexionForm', connexionForm);
-        app.use('/', index);
-        app.set('views', './views');
-        app.set('view engine', 'ejs');
-        app.use('/public', express.static('public'));
-    }
-})
+// db.connect(function(err){
+//     if (err) {
+//         console.log(err)
+//     }else{
+//         app.use('/rejoindre', rejoindre);
+//         app.use('/contact', contact);
+//         app.use('/connexionForm', connexionForm);
+//         app.use('/', index);
+//         app.set('views', './views');
+//         app.set('view engine', 'ejs');
+//         app.use('/public', express.static('public'));
+//     }
+// })
 
+app.use('/rejoindre', rejoindre);
+app.use('/contact', contact);
+app.use('/connexionForm', connexionForm);
+app.use('/', index);
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
 
 
